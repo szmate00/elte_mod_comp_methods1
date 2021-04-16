21b6eb900e2626aa0ee56c4928d911ec37b6f0a4
Szűcs Máté <szcsm2000@gmail.com>
2021-04-16 10:05:26 +0200

    small updates

--- Homeworks/Homework 3/main.cpp
+++ Homeworks/Homework 3/main.cpp
@@ -7,19 +7,6 @@ using std::cout;
 using std::endl;
 
 
-/*
-    |0.01 error for the integration methods|
-
-Max of abs(f''(x)) at [0, 200] -> M = 0.00216214
-Max of abs(f''''(x)) at [0, 200] -> M_4 = 0.00000000864858
-
-- Midpoint: (0.00216214 * 200^3) / (24 * n^2) = 0.01 -> n = 269
-- Trapezoidal: (0.00216214 * 200^3) / (12 * n^2) = 0.01 -> n = 380
-- 1/3 Simpson's: (200^5) / (2880 * n^4) * 0.00000000864858 = 0.01 -> n = 4
-
-*/
-
-
 // simple test function to check goodness of integrations
 double function(double x) 
 {
